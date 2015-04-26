bd92a127e6823a0dd709e6b98c58d9cfda48e7e0
Jay Peterson <jay.peterson@live.com>
2015-04-25 19:43:56 -0700

    Added V# buttons and time since last climb

---
+++ Instaclimb/Problem.swift
@@ -0,0 +1,15 @@
+//
+//  Problem.swift
+//  Instaclimb
+//
+//  Created by Jay Peterson on 4/25/15.
+//  Copyright (c) 2015 JL Corp. All rights reserved.
+//
+
+import Foundation
+
+class Problem {
+  var grade: Int?
+  var dates: [NSDate]?
+  
+}
--- Instaclimb/ViewController.swift
+++ Instaclimb/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBOutlet weak var timerLabel: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  var timer: NSTimer!
+  var prevTime: NSDate?
+  var problems = [Problem]()
 
+  @IBAction func vButtonPressed(sender: UIButton) {
+    println(sender.titleLabel?.text)
+    prevTime = NSDate()
+
+    var text  = sender.titleLabel!.text!
+    text.replaceRange(Range(start: text.startIndex, end: advance(text.startIndex, 1)), with: "")
+    var grade = text.toInt()!
+
+    let alert = UIAlertController(title: "Add V\(text)?", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+    let addSimpleAlertAction = UIAlertAction(title: "Add", style: UIAlertActionStyle.Default, handler: makeAddSimpleHandler(grade))
+
+    alert.addAction(addSimpleAlertAction)
+    alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { (action) in }))
+    presentViewController(alert, animated: true, completion: nil)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTimer", userInfo: nil, repeats: true)
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func updateTimer() {
+    if let prevTime = prevTime {
+      let now = NSDate()
+      let interval = now.timeIntervalSinceDate(prevTime)
+      let dateFormatter = NSDateFormatter()
+      dateFormatter.dateFormat = "mm:ss"
+      timerLabel.text = dateFormatter.stringFromDate(NSDate(timeIntervalSince1970: interval))
+    }
+  }
+
+  func makeAddSimpleHandler(grade: Int) -> ((UIAlertAction!) ->Void) {
+    func handler(action: UIAlertAction!) {
+      println(grade)
+    }
+    return handler
+  }
 
 }
 
